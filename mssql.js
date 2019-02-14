8b0fb18ef028b7ac6bc31041d880236c0a048dd1
Kimson <kimyeh@hk1.ibm.com>
2019-02-14 17:07:40 +0800

    real pool connection

--- mssql.js
+++ mssql.js
@@ -1,51 +1,67 @@
 module.exports = function(RED) {
-	'use strict';
-	var mustache = require('mustache');
-	var sql = require('mssql');
+  'use strict';
+  var mustache = require('mustache');
+  var sql = require('mssql');
+  
+  var pool = null;
 
-	function connection(config) {
-	    RED.nodes.createNode(this, config);
+  function connection(config) {
+      RED.nodes.createNode(this, config);
 
       var node = this;
-	    this.config = {
+      this.config = {
             user: node.credentials.username,
             password: node.credentials.password,
-						domain: node.credentials.domain,
+            domain: node.credentials.domain,
             server: config.server,
+            port: Number(config.port),
             database: config.database,
             options: {
-                           encrypt : config.encyption,
+                           encrypt : config.encyption || true,
                            useUTC: config.useUTC
-                       }
+                       },
+            pool: {
+                max: config.pool,
+                min: 0,
+                idleTimeoutMillis: 30000
+            },
         };
 
 
-	    this.connection = sql;
-			/*
-			node.on('close',function(){
-   			node.pool.close(function(){});
-   		})
-			*/
-		}
+      this.connection = sql;
+	  //node.pool = null;
+	  console.log('config:'+JSON.stringify(this.config));
+	  pool = new this.connection.ConnectionPool(this.config);
+	  //pool = new sql.ConnectionPool(this.config);
+      pool.connect();
+      //node.on('close',function(){
+      //  node.pool.close(function(){});
+      //})
 
-  	RED.nodes.registerType('MSSQL-CN', connection, {
-	    credentials: {
-				username: {type:'text'},
-				password: {type:'password'},
-				domain: {type: 'text'}
-	    }
+    }
+
+    RED.nodes.registerType('MSSQL-CN', connection, {
+      credentials: {
+        username: {type:'text'},
+        password: {type:'password'},
+        domain: {type: 'text'}
+      }
 	});
+	
+	//pool.on('error', err => {
+	//	console.log("pool on:"+err)
+	//})
 
-  	function mssql(config) {
-	    RED.nodes.createNode(this, config);
-	    var mssqlCN = RED.nodes.getNode(config.mssqlCN);
-	    this.query = config.query;
-	    this.connection = mssqlCN.connection;
-	    this.config = mssqlCN.config;
-	    this.outField = config.outField;
+    function mssql(config) {
+      RED.nodes.createNode(this, config);
+      var mssqlCN = RED.nodes.getNode(config.mssqlCN);
+      this.query = config.query;
+      this.connection = mssqlCN.connection;
+      this.config = mssqlCN.config;
+      this.outField = config.outField;
 
       var node = this;
-	    var b = node.outField.split('.');
+      var b = node.outField.split('.');
       var i = 0;
       var r = null;
       var m = null;
@@ -69,38 +85,80 @@ module.exports = function(RED) {
 
       node.on('input',function(msg){
           console.log(node.config);
-
-          node.connection.connect(node.config).then(function(){
-
-            node.status({fill:'blue',shape:'dot',text:'requesting'});
-
-            var query = mustache.render(node.query,msg);
-
-            if (!query || (query === '')) {
-                query = msg.payload;
-            }
-
-            var request = new node.connection.Request();
-
-            request.query(query).then(function (rows){
-        		i = 0;
-        		r = rows;
-        		m = msg;
-        		rec(msg);
-            }).catch(function(err) {
-                node.error(err);
-                node.status({fill:'red',shape:'ring',text:'Error'});
-                return;
-            });
-
-
-        }).catch(function(err) {
-    		node.error(err);
-    		node.status({fill:'red',shape:'ring',text:'Error'});
-    		return;
-        });
+			
+		//####
+		pool.on('error', err => {
+			console.log("pool on:"+err);
+		})
+		console.log(pool);
+		const request = pool.request();
+		let temp_query = '';
+		if(node.query){
+			temp_query = node.query;
+		}else if(msg.query){
+			temp_query = msg.query;
+		}else{
+			temp_query = '';
+		}
+		//var query = mustache.render(node.query,msg);
+		var query = mustache.render(temp_query,msg);
+		console.log(query);
+		if (!query || (query === '')) {
+			query = msg.payload;
+		}
+		request.query(query).then(function (rows){
+			i = 0;
+			r = rows;
+			m = msg;
+			rec(msg);
+		}).catch(function(err) {
+			console.log(err);
+			node.error(err);
+			node.status({fill:'red',shape:'ring',text:'Error'});
+			return;
+		});
+		//####
+		//org is not using pool
+		//  node.connection.connect(node.config).then(function(){
+        //
+        //    node.status({fill:'blue',shape:'dot',text:'requesting'});
+		//	
+		//	let temp_query = '';
+		//	if(node.query){
+		//		temp_query = node.query;
+		//	}else if(msg.query){
+		//		temp_query = msg.query;
+		//	}else{
+		//		temp_query = '';
+		//	}
+        //    //var query = mustache.render(node.query,msg);
+		//	var query = mustache.render(temp_query,msg);
+		//	console.log(query);
+        //    if (!query || (query === '')) {
+        //        query = msg.payload;
+        //    }
+        //
+        //    var request = new node.connection.Request();
+        //
+        //    request.query(query).then(function (rows){
+        //    i = 0;
+        //    r = rows;
+        //    m = msg;
+        //    rec(msg);
+        //    }).catch(function(err) {
+        //        node.error(err);
+        //        node.status({fill:'red',shape:'ring',text:'Error'});
+        //        return;
+        //    });
+        //
+        //
+        //}).catch(function(err) {
+        //node.error(err);
+        //node.status({fill:'red',shape:'ring',text:'Error'});
+        //return;
+        //});
       });
 
-	}
-  	RED.nodes.registerType('MSSQL', mssql);
+  }
+    RED.nodes.registerType('MSSQL', mssql);
 };
